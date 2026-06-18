0979e7544ae7ef0d00a0f8b2778d9473e7d532ec
chefcalvinkitchen <amenajuli@gmail.com>
2026-06-18 15:21:40 +0600

    Update intake.js

--- api/intake.js
+++ api/intake.js
@@ -1,37 +1,8 @@
 export default async function handler(req, res) {
 
-  try {
-
-    const SHOP = process.env.SHOPIFY_SHOP;
-
-    const response = await fetch(
-      `https://${SHOP}.myshopify.com/admin/oauth/access_token`,
-      {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/x-www-form-urlencoded'
-        },
-        body: new URLSearchParams({
-          grant_type: 'client_credentials',
-          client_id: process.env.SHOPIFY_CLIENT_ID,
-          client_secret: process.env.SHOPIFY_CLIENT_SECRET
-        })
-      }
-    );
-
-    const text = await response.text();
-
-    return res.status(200).send({
-      status: response.status,
-      response: text
-    });
-
-  } catch (error) {
-
-    return res.status(500).json({
-      error: error.message
-    });
-
-  }
+  return res.status(200).json({
+    SHOPIFY_STORE: process.env.SHOPIFY_STORE || null,
+    SHOPIFY_SHOP: process.env.SHOPIFY_SHOP || null
+  });
 
 }
