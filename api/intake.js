a4ef4f4538f15567ebe831abe6d9376c83b45bc6
chefcalvinkitchen <amenajuli@gmail.com>
2026-06-18 08:50:58 +0600

    Update intake.js

--- api/intake.js
+++ api/intake.js
@@ -1,6 +1,47 @@
 export default async function handler(req, res) {
-  res.status(200).json({
-    success: true,
-    message: "Chef Calvin Intake API is working"
+
+  res.setHeader(
+    'Access-Control-Allow-Origin',
+    'https://www.chefcalvinskitchen.com'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Methods',
+    'GET,POST,OPTIONS'
+  );
+
+  res.setHeader(
+    'Access-Control-Allow-Headers',
+    'Content-Type'
+  );
+
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+
+  if (req.method === 'GET') {
+
+    return res.status(200).json({
+      success: true,
+      message: 'Chef Calvin Intake API is working'
+    });
+
+  }
+
+  if (req.method === 'POST') {
+
+    console.log('Form Submission:', req.body);
+
+    return res.status(200).json({
+      success: true,
+      received: true
+    });
+
+  }
+
+  return res.status(405).json({
+    success: false,
+    message: 'Method not allowed'
   });
+
 }
